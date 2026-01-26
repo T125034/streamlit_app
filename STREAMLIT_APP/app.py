6c46b52ecadca194e1420cbc4dba689789629462
T125034 <t125034@ed.sus.ac.jp>
2026-01-26 10:39:06 +0900

    サイドバー追加

--- STREAMLIT_APP/app.py
+++ STREAMLIT_APP/app.py
@@ -3,5 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-st.title('出入国管理統計 出入（帰）国者数（2024年）')
-df=pd.read_csv('FEH_00250011_260126095056.csv')
+st.title('人口動態調査 人口動態統計 確定数 出生')
+df=pd.read_csv('FEH_00450011_260126102708.csv')
+
+with st.sidebar:
+    st.subheader('抽出条件')
+    goods=st.multiselect('年代を選択してください（複数選択可）',
+                          df['時間軸(年次)'].unique())
