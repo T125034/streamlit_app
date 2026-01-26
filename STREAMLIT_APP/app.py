5c6f3776c4d822cca53121e1dfaaf40e00b01270
T125034 <t125034@ed.sus.ac.jp>
2026-01-26 09:55:59 +0900

    ファイル読み込み

--- STREAMLIT_APP/app.py
+++ STREAMLIT_APP/app.py
@@ -1,3 +1,7 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
 
-st.write("test")
+st.title('出入国管理統計 出入（帰）国者数（2024年）')
+df=pd.read_csv('FEH_00250011_260126095056.csv')
