b28f96b658408af43703082db891bc07a72a7d49
T125034 <t125034@ed.sus.ac.jp>
2026-01-26 11:04:10 +0900

    修正

--- STREAMLIT_APP/app.py
+++ STREAMLIT_APP/app.py
@@ -10,25 +10,29 @@ with st.sidebar:
     st.subheader('抽出条件')
     year=st.multiselect('年代を選択してください（複数選択可）',
                           df['時間軸(年次)'].unique())
-    st.subheader('色分け')
-    color=st.selectbox('分類を選択してください',
-                       ['合計','男','女'])
-    
-if color == '合計':
-    color_column = '出生数_総数【人】'
-elif color == '男':
-    color_column = '出生数_男【人】'
-elif color == '女':
-    color_column = '出生数_女【人】'
     
 df=df[df['時間軸(年次)'].isin(year)]
 
 st.dataframe(df,width=600,height=200)
 
+df_long = df.melt(
+    id_vars='時間軸(年次)',
+    value_vars=['出生数_総数【人】', '出生数_男【人】', '出生数_女【人】'],
+    var_name='分類',
+    value_name='出生数'
+)
+
+df_long['分類'] = df_long['分類'].replace({
+    '出生数_総数【人】': '合計',
+    '出生数_男【人】': '男',
+    '出生数_女【人】': '女'
+})
+
+
 fig=px.scatter(df,
                x='時間軸(年次)',
                y='出生数_総数【人】',
-               color=color_column,
+               color='分類',
                labels={'時間軸(年次)': '年代', '出生数_総数【人】': '出生数'},
                title='年代別出生数',
                trendline='ols')
